d7a77c442d97a2bd749a30fda2ea9465befe99a5
Ben Dabush <bendab2304@gmail.com>
2023-01-12 20:02:14 +0200

    Added getCurrentMax test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -6,14 +6,13 @@ import java.util.concurrent.*;
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
     @Test
-    public void partialTest() {
+    public void partialTest()
+    {
         CustomExecutor customExecutor = new CustomExecutor();
-//        for (int j = 0; j < 10; j++) {
-
 
             var task = Task.createTask(() -> {
                 int sum = 0;
-                for (int i = 1; i <= 10; i++) {
+                for (int i = 1; i <= 10000; i++) {
                     sum += i;
                 }
                 return sum;
@@ -26,31 +25,118 @@ public class Tests {
                 throw new RuntimeException(e);
             }
             logger.info(() -> "Sum of 1 through 10 = " + sum);
-//        }
-        Callable<Double> callable1 = () -> {
-            return 1000 * Math.pow(1.02, 5);
-        };
-        Callable<String> callable2 = () -> {
-            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            return sb.reverse().toString();
-        };
-        // var is used to infer the declared type automatically
-        var priceTask = customExecutor.submit(() -> {
-            return 1000 * Math.pow(1.02, 5);
-        }, TaskType.COMPUTATIONAL);
-        var reverseTask = customExecutor.submit(callable2, TaskType.IO);
-        final Double totalPrice;
-        final String reversed;
-        try {
-            totalPrice = priceTask.get();
-            reversed = reverseTask.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-        logger.info(() -> "Reversed String = " + reversed);
-        logger.info(() -> String.valueOf("Total Price = " + totalPrice));
-        logger.info(() -> "Current maximum priority = " +
-                customExecutor.getCurrentMax());
+
+            Callable<Double> callable1 = () -> {
+                return 1000 * Math.pow(1.021279, 10);
+            };
+            Callable<String> callable2 = () -> {
+                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                return sb.reverse().toString();
+            };
+            // var is used to infer the declared type automatically
+            var priceTask = customExecutor.submit(() -> {
+                return 1000 * Math.pow(1.029381940, 20);
+            }, TaskType.COMPUTATIONAL);
+            var reverseTask = customExecutor.submit(callable2, TaskType.IO);
+            final Double totalPrice;
+            final String reversed;
+            try {
+                totalPrice = priceTask.get();
+                reversed = reverseTask.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+            logger.info(() -> "Reversed String = " + reversed);
+            logger.info(() -> String.valueOf("Total Price = " + totalPrice));
+        logger.info(()-> "Current maximum priority = " + customExecutor.getCurrentMax());
         customExecutor.gracefullyTerminate();
     }
+
+
+    @Test
+    public void getCurrentMaxTest(){
+
+        CustomExecutor customExecutor = new CustomExecutor();
+
+        var task = Task.createTask(() -> {
+            int sum = 0;
+            for (int i = 1; i <= 1000; i++) {
+                sum += i;
+            }
+            return sum;
+        }, TaskType.COMPUTATIONAL);
+
+        for(int i =0 ; i < 10; i++)
+        {
+            task.getType().setPriority((i%9)+1);
+            var sumTask = customExecutor.submit(task);
+            final int sum;
+            try {
+                sum = sumTask.get(100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+//            if (i%100==0){
+//
+//                logger.info(()-> "Current maximum priority = " + customExecutor.getCurrentMax());
+//            }
+        }
+
+//        logger.info(()-> "Current maximum priority = " + customExecutor.getCurrentMax());
+        customExecutor.gracefullyTerminate();
+    }
+
 }
+
+
+
+
+
+//    public void naamaTest() {
+        //        for (int j = 0; j < 10; j++) {
+
+
+
+//            var task = Task.createTask(() -> {
+//                int sum = 0;
+//                for (int i = 1; i <= 1000; i++) {
+//                    sum += i;
+//                }
+//                return sum;
+//            }, TaskType.COMPUTATIONAL);
+//            var sumTask = customExecutor.submit(task);
+//            final int sum;
+//            try {
+//                sum = sumTask.get(1, TimeUnit.MILLISECONDS);
+//            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+//                throw new RuntimeException(e);
+//            }
+//            logger.info(() -> "Sum of 1 through 10 = " + sum);
+////        }
+//        Callable<Double> callable1 = () -> {
+//            return 1000 * Math.pow(1.02, 5);
+//        };
+//        Callable<String> callable2 = () -> {
+//            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//            return sb.reverse().toString();
+//        };
+//        // var is used to infer the declared type automatically
+//        var priceTask = customExecutor.submit(() -> {
+//            return 1000 * Math.pow(1.02, 5);
+//        }, TaskType.COMPUTATIONAL);
+//        var reverseTask = customExecutor.submit(callable2, TaskType.IO);
+//        final Double totalPrice;
+//        final String reversed;
+//        try {
+//            totalPrice = priceTask.get();
+//            reversed = reverseTask.get();
+//        } catch (InterruptedException | ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+//        logger.info(() -> "Reversed String = " + reversed);
+//        logger.info(() -> String.valueOf("Total Price = " + totalPrice));
+//        logger.info(() -> "Current maximum priority = " +
+//                customExecutor.getCurrentMax());
+//        customExecutor.gracefullyTerminate();
+//    }
+//}
