3e7a0021aee4e9620e4023711c4f361cffedc028
Ben Dabush <bendab2304@gmail.com>
2023-01-12 04:47:09 +0200

    Adding comments and removing Callable from implements of the class and what is related to it

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,45 +1,86 @@
 import org.jetbrains.annotations.NotNull;
-
 import java.util.concurrent.*;
 
-public class Task<T> extends FutureTask<T> implements Callable<T>, Comparable<Task<T>> {
+/**
+ * A Task class that extends {@link FutureTask} and implements {@link Comparable}.
+ *
+ * @author Ben Dabush and Naana Shiponi
+ * @version 1.0.0 January 12, 2023
+ */
+public class Task<T> extends FutureTask<T> implements Comparable<Task<T>> {
     private final TaskType type;
 
-    public Task(@NotNull Callable<T> callable,TaskType type) {
+    /**
+     * Constructor for the Task class.
+     *
+     * @param callable The {@link Callable} object that the task will execute.
+     * @param type The {@link TaskType} of the task.
+     */
+    private Task(@NotNull Callable<T> callable,TaskType type) {
         super(callable);
         this.type=type;
-
     }
 
+    /**
+     * Returns the {@link TaskType} of the task.
+     *
+     * @return The type of the task.
+     */
     public TaskType getType() {
         return type;
     }
 
-
+    /**
+     * A factory method for creating a Task object with a {@link Callable} and {@link TaskType}.
+     *
+     * @param call The {@link Callable} that the task will execute.
+     * @param type The {@link TaskType} of the task.
+     * @return A new Task object with the given {@link Callable} and {@link TaskType}.
+     */
     public static <T> Task<T> createTask(Callable<T> call, TaskType type){
-
         return new Task<T>(call,type);
     }
-    public static <T> Task<T> createTask(Callable<T> call){
 
+    /**
+     * A factory method for creating a Task object with a {@link Callable} with {@link TaskType} "OTHER".
+     *
+     * @param call The {@link Callable} that the task will execute.
+     * @return A new Task object with the given {@link Callable} and {@link TaskType} "OTHER".
+     */
+    public static <T> Task<T> createTask(Callable<T> call){
         return new Task<T>(call,TaskType.OTHER);
     }
 
-    @Override
-    public T call() throws Exception {
-            return this.call();
-    }
-
-
+    /**
+     * Compares this task to another task based on the priority value of their respective {@link TaskType}.
+     *
+     * @param otherTask The task to compare this task to.
+     * @return A negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     */
     @Override
     public int compareTo(@NotNull Task<T> otherTask) {
-        if (this.type.getPriorityValue()<otherTask.getType().getPriorityValue()){
-            return -1;
-        }else if(this.type.getPriorityValue()==otherTask.getType().getPriorityValue()){
-            return 0;
-        }else return 1;
+        return Integer.compare(this.type.getPriorityValue(), otherTask.getType().getPriorityValue());
+    }
 
+    /**
+     * Indicates whether some other object is "equal to" this task.
+     *
+     * @param obj the reference object with which to compare.
+     * @return true if this object is the same as the obj argument; false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 
+    /**
+     * Returns a hash code value for the task.
+     *
+     * @return a hash code value for this task.
+     */
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 
 //    @Override
